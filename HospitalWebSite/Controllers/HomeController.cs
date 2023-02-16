c221e2a98725a8d637b12192c214f87c29bce7a4
Normand <normandlaredo@gmail.com>
2023-02-16 10:21:23 +0000

    [IMP] adding boxes for book appointment in hero

--- HospitalWebSite/Controllers/HomeController.cs
+++ HospitalWebSite/Controllers/HomeController.cs
@@ -18,26 +18,6 @@ namespace HospitalWebSite.Controllers
             return View();
         }
 
-        public IActionResult Services()
-        {
-            return View();
-        }
-
-        public IActionResult Doctors()
-        {
-            return View();
-        }
-
-        public IActionResult News()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
